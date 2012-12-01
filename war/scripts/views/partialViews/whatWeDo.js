a10ffd0d146a5156b1369381209a6edfcaa06fa6
Nahuel Barrios <barrios.nahuel@gmail.com>
2012-12-01 18:52:41 -0300

    Arreglo el cambio de idioma también para las pestañas del qué hacemos que ahí no estaba andando.

--- war/scripts/views/partialViews/whatWeDo.js
+++ war/scripts/views/partialViews/whatWeDo.js
@@ -2,41 +2,65 @@ var views = views || {};
 
 views.whatWeDo = function() {
 
-	var info = function(callback) {
-		$.getJSON("data/whatWeDo-" + site.language + ".json", function(json) {
-			callback(json);
-		});
-	};
+    var info = function(callback) {
+        $.getJSON("data/whatWeDo-" + site.language + ".json", function(json) {
+            callback(json);
+        });
+    };
 
-	var init = function(sectionId) {
+    var init = function(sectionId) {
+        var sectionToShow;
 
-		var sectionToShow;
-
-		switch (sectionId) {
-		case 11:
-			sectionToShow = $('#sectionsTab a:first');
-			break;
-		case 12:
-			sectionToShow = $('#sectionsTab li:eq(1) a');
-			break;
-		case 13:
-		    sectionToShow = $('#sectionsTab li:eq(2) a');
-			break;
-		case 14:
+        switch (sectionId) {
+        case 11:
+            sectionToShow = $('#sectionsTab a:first');
+            break;
+        case 12:
+            sectionToShow = $('#sectionsTab li:eq(1) a');
+            break;
+        case 13:
+            sectionToShow = $('#sectionsTab li:eq(2) a');
+            break;
+        case 14:
             sectionToShow = $('#sectionsTab a:last');
             break;
-		default:
-			sectionToShow = $('#sectionsTab a:first');
-			break;
-		}
+        default:
+            sectionToShow = $('#sectionsTab a:first');
+            break;
+        }
 
-		sectionToShow.tab("show");
-	};
+        sectionToShow.tab("show");
 
-	return {
-		info : info,
-		url : "whatWeDo.html",
-		templateScriptId : "#whatWeDoTemplate",
-		init : init
-	};
+        // Setteo el site.currentPage para cuando el usuario cambia de pestaña
+        // por si luego quiere cambiar de idioma.
+        $("#sectionsTab li a").bind("click", function() {
+            var href = this.href;
+            var tabId = href.substring(href.indexOf('#') + 1);
+
+            switch (tabId) {
+            case "adults":
+                site.currentPage = 11;
+                break;
+            case "psico":
+                site.currentPage = 12;
+                break;
+            case "training":
+                site.currentPage = 13;
+                break;
+            case "mindfulness":
+                site.currentPage = 14;
+                break;
+            default:
+                site.currentPage = 11;
+                break;
+            }
+        });
+    };
+
+    return {
+        info : info,
+        url : "whatWeDo.html",
+        templateScriptId : "#whatWeDoTemplate",
+        init : init
+    };
 };
